cf68f47a4f78da1cf735d1b1889a796ba0696628
김지훈 <ji_hun23@naver.com>
2022-07-25 22:47:01 +0900

    9012 Solve

---
+++ Stack/Stack9012.java
@@ -0,0 +1,43 @@
+package practice.Backjoon.Control;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Stack9012 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Character> s = new Stack<Character>();
+		Scanner sc = new Scanner(System.in);
+		String str;
+		int T = sc.nextInt();
+		for(int t=0; t<T; t++)
+		{
+			str = sc.next();
+			String result = "YES";
+			for(int i=0; i<str.length(); i++) {
+				if(str.charAt(i) == '(') {
+					s.push('(');
+				}
+				else {
+					if(s.isEmpty()) {
+						result = "NO";
+						break;
+					}
+					else {
+						s.pop();
+					}
+				}
+			}
+			if(!s.isEmpty()) {
+				result = "NO";
+				while(!s.isEmpty()) {
+					s.pop();
+				}
+			}
+			
+			System.out.println(result);
+		}
+	}
+
+}
